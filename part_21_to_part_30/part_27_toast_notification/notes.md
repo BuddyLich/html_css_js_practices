3fa5edac8580ac472746e20ca04680617d7fb8ac
Zhen <zhengzhen048@gmail.com>
2021-02-20 18:19:39 +1100

    chapter 27 - complete the chapter

---
+++ part_21_to_part_30/part_27_toast_notification/notes.md
@@ -0,0 +1,4 @@
+## Notes
+
+### CSS:
+1. In flex box, you can use :`align-items: flex-end;` to make things align to the end
---
+++ part_21_to_part_30/part_27_toast_notification/script.js
@@ -0,0 +1,28 @@
+const button = document.querySelector('#button')
+const toasts = document.querySelector('.toasts')
+
+const messages = [
+    'Message One',
+    'Message Two',
+    'Message Three',
+    'Message Four',
+    'Message Five',
+]
+
+button.addEventListener('click', createNotification)
+
+function createNotification() {
+    let notif = document.createElement('div')
+    notif.classList.add('toast')
+
+    notif.innerText = getRandomMessage()
+    toasts.appendChild(notif)
+
+    setTimeout(() => {
+        notif.remove()
+    }, 3000)
+}
+
+function getRandomMessage() {
+    return messages[Math.floor(Math.random() * messages.length)]
+}
